3efec383f4b9381b5ad89870d07944bbe6d6ca49
Julien Maupetit <julien@tailordev.fr>
2016-05-19 15:37:47 +0200

    Add support for sequence nucleotides selection

--- app/components/Nucleotide.jsx
+++ app/components/Nucleotide.jsx
@@ -1,48 +1,58 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const { number, string } = PropTypes;
+const { bool, func, number, string } = PropTypes;
 
-const Nucleotide = (props) => (
-  <g
-    className="nucleotide"
-    title="click to start selection"
-    transform={`translate(${props.x}, ${props.y})`}
-  >
-    <text x="5" y="43" fill="#333">{props.type}</text>
+export default class Nucleotide extends Component {
 
-    <g
-      className="position"
-    >
-      <rect
-        className="background"
-        x={-(props.position.toString().length - 1) * 10 / 2}
-        y="0"
-        width={props.position.toString().length * 10 + 10}
-        height="25"
-        rx="2"
-        ry="2"
-      />
+  shouldComponentUpdate(nextProps) {
+    return this.props.isSelected !== nextProps.isSelected;
+  }
 
-      <rect
-        className="border"
-        x="0"
-        y="20"
-        width="20"
-        height="30"
-        rx="5"
-        ry="5"
-      />
-
-      <text
-        x={5 - ((props.position.toString().length - 1) * 10 / 2)}
-        y="18"
+  render() {
+    return (
+      <g
+        className="nucleotide"
+        title="click to start selection"
+        transform={`translate(${this.props.x}, ${this.props.y})`}
+        onClick={this.props.onClick}
       >
-        {props.position}
-      </text>
+        <text x="5" y="43" fill="#333">{this.props.type}</text>
 
-    </g>
-  </g>
-);
+        <g
+          className={this.props.isSelected ? 'position selected' : 'position'}
+        >
+          <rect
+            className="background"
+            x={-(this.props.position.toString().length - 1) * 10 / 2}
+            y="0"
+            width={this.props.position.toString().length * 10 + 10}
+            height="25"
+            rx="2"
+            ry="2"
+          />
+
+          <rect
+            className="border"
+            x="0"
+            y="20"
+            width="20"
+            height="30"
+            rx="5"
+            ry="5"
+          />
+
+          <text
+            x={5 - ((this.props.position.toString().length - 1) * 10 / 2)}
+            y="18"
+          >
+            {this.props.position}
+          </text>
+
+        </g>
+      </g>
+    );
+  }
+}
 
 Nucleotide.propTypes = {
   x: number.isRequired,
@@ -50,6 +60,7 @@ Nucleotide.propTypes = {
   // attributes
   type: string.isRequired,
   position: number.isRequired,
+  isSelected: bool.isRequired,
+  // events
+  onClick: func.isRequired,
 };
-
-export default Nucleotide;
--- app/components/Sequence.jsx
+++ app/components/Sequence.jsx
@@ -1,27 +1,56 @@
-import React, { PropTypes } from 'react';
+import Immutable from 'immutable';
+import React, { Component, PropTypes } from 'react';
 
 import Nucleotide from './Nucleotide';
 
 const { array } = PropTypes;
 
+export default class Sequence extends Component {
+  constructor(props, context) {
+    super(props, context);
 
-const Sequence = (props) => (
-  <g>
-    {props.sequence.map((nucleotide, index) =>
-      <Nucleotide
-        x={5 + (12 * index)}
-        y={30}
-        type={nucleotide}
-        position={index + 1}
-        key={index}
-      />
-    )}
-  </g>
-);
+    this.state = {
+      selection: new Immutable.OrderedSet(),
+    };
+  }
+
+  updateSelection(selected) {
+    this.setState({
+      selection: this.state.selection.has(selected) ?
+        this.state.selection.delete(selected) :
+        this.state.selection.add(selected),
+    });
+  }
+
+  handleNucleotideClick(newIndex) {
+    this.updateSelection(newIndex);
+  }
+
+  render() {
+    return (
+      <g>
+        {
+          this.props.sequence.map((nucleotide, index) => {
+            const boundClick = this.handleNucleotideClick.bind(this, index);
+            return (
+              <Nucleotide
+                x={5 + (12 * index)}
+                y={30}
+                type={nucleotide}
+                position={index + 1}
+                key={index}
+                isSelected={this.state.selection.includes(index)}
+                onClick={boundClick}
+              />
+            );
+          })
+        }
+      </g>
+    );
+  }
+}
 
 Sequence.propTypes = {
   sequence: array.isRequired,
   // TODO: add other attrs, cf. https://github.com/TailorDev/franklin/issues/3
 };
-
-export default Sequence;
