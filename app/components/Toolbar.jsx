98632b82070d5852e3f908c23da915f66a3571e6
Julien Maupetit <julien@tailordev.fr>
2016-06-02 17:46:52 +0200

    Remove un-implemented mockups

--- app/components/Toolbar.jsx
+++ app/components/Toolbar.jsx
@@ -8,17 +8,5 @@ export default (props) =>
       <h4>Labels</h4>
       <Labels {...props} />
     </div>
-
-    <div className="search-panel">
-      <h4>Search</h4>
-
-      <input type="text" id="search" placeholder="type your pattern here" />
-    </div>
-
-    <div className="export-panel">
-      <h4>Export</h4>
-
-      <button className="button">Download SVG</button>
-    </div>
   </div>
 ;
