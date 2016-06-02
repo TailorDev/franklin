d83f66a8ca4734a5faf896f80ef9b80151a5d201
Julien Maupetit <julien@tailordev.fr>
2016-06-02 16:04:44 +0200

    Add tests for the LabelRemove component

---
+++ app/components/__tests__/LabelRemove-test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+// see: https://github.com/mochajs/mocha/issues/1847
+const { describe, it } = global;
+
+import LabelRemove from '../LabelRemove';
+
+
+describe('<LabelRemove />', () => {
+
+  it('renders a remove dialog box', () => {
+    const wrapper = shallow(
+      <LabelRemove
+        onActionRemoveCancelClick={() => {}}
+        onLabelRemove={() => {}}
+      />
+    );
+
+    expect(wrapper.find('.action-remove')).to.have.length(1);
+    expect(wrapper.find('button.remove')).to.have.length(1);
+    expect(wrapper.find('button.cancel')).to.have.length(1);
+  });
+
+  it('fires label deletion canceling', () => {
+    const spy = sinon.spy();
+    const wrapper = shallow(
+      <LabelRemove
+        onActionRemoveCancelClick={spy}
+        onLabelRemove={() => {}}
+      />
+    );
+
+    wrapper.find('button.cancel').simulate('click');
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('fires label deletion', () => {
+    const spy = sinon.spy();
+    const wrapper = shallow(
+      <LabelRemove
+        onActionRemoveCancelClick={() => {}}
+        onLabelRemove={spy}
+      />
+    );
+
+    wrapper.find('button.remove').simulate('click');
+    expect(spy.calledOnce).to.be.true;
+  });
+});
