40a2c02c5b090f5b512f35a086e8a0a8fd965268
William Durand <will+git@drnd.me>
2016-06-29 16:44:08 +0200

    Introduce Input component

---
+++ app/components/AnnotationForm/Input.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+const Input = (props) => {
+  let isInvalid = false;
+  if (!isNaN(parseInt(props.value, 10))) {
+    isInvalid = props.value < props.min;
+  }
+
+  return (
+    <label className={isInvalid ? 'is-invalid-label' : null}>
+      <input
+        type="number"
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        disabled={props.isDisabled}
+        min={props.min}
+        className={isInvalid ? 'is-invalid-input' : null}
+      />
+      <small className={`form-error${isInvalid ? ' is-visible' : ''}`}>
+        {props.errorText || 'Invalid value'}
+      </small>
+    </label>
+  );
+};
+
+Input.propTypes = {
+  value: React.PropTypes.number.isRequired,
+  min: React.PropTypes.number.isRequired,
+  placeholder: React.PropTypes.string.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  isDisabled: React.PropTypes.bool.isRequired,
+  errorText: React.PropTypes.string,
+};
+
+export default Input;
