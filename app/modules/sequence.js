3faaa0e90411061d85657a8812ed3eae27aa4733
William Durand <will+git@drnd.me>
2016-07-01 00:24:50 +0200

    Remove a TODO

--- app/modules/sequence.js
+++ app/modules/sequence.js
@@ -64,7 +64,6 @@ export default function reducer(state = initialState, action = {}) {
       return {
         name: action.name,
         sequence: action.sequence,
-        // TODO: allow user input for from/to positions (at least from)
         positionFrom: 1,
         loading: false,
       };
