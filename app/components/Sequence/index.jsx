0a599153a709ca6289000e60ba988d2ab612e0fa
William Durand <will+git@drnd.me>
2016-07-04 17:27:49 +0200

    Avoid too many Sequence updates

--- app/components/Sequence/index.jsx
+++ app/components/Sequence/index.jsx
@@ -1,24 +1,36 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Immutable from 'immutable';
 
 import Nucleotide from '../Nucleotide';
 
 const { func, object, instanceOf, number } = PropTypes;
 
-const Sequence = (props) => (
-  <g>
-    {props.sequence.map((nucleotide, index) =>
-      <Nucleotide
-        type={nucleotide}
-        position={props.positionFrom + index}
-        key={index}
-        index={index}
-        onClick={props.onNucleotideClick}
-        {...props}
-      />
-    )}
-  </g>
-);
+class Sequence extends Component {
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.positionFrom !== nextProps.positionFrom ||
+      this.props.sequence !== nextProps.sequence ||
+      this.props.nucleotidesPerRow !== nextProps.nucleotidesPerRow ||
+      this.props.rowHeight !== nextProps.rowHeight;
+  }
+
+  render() {
+    return (
+      <g>
+        {this.props.sequence.map((nucleotide, index) =>
+          <Nucleotide
+            type={nucleotide}
+            position={this.props.positionFrom + index}
+            key={index}
+            index={index}
+            onClick={this.props.onNucleotideClick}
+            {...this.props}
+          />
+        )}
+      </g>
+    );
+  }
+}
 
 Sequence.propTypes = {
   sequence: instanceOf(Immutable.List).isRequired,
